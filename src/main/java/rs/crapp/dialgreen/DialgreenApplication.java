bed8894b2d778c7605efd186b798701a16f12c5e
dimitrije_dm <dimitrije.milenkovic@netcast.rs>
2018-01-27 10:59:14 +0100

    fc

---
+++ src/main/java/rs/crapp/dialgreen/DialgreenApplication.java
@@ -0,0 +1,12 @@
+package rs.crapp.dialgreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialgreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DialgreenApplication.class, args);
+	}
+}
