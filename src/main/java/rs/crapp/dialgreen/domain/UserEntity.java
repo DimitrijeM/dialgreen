47df89f347db01e7e3b7173464529489db1a6e6c
dimitrije_dm <dimitrije.milenkovic@netcast.rs>
2018-01-27 11:45:09 +0100

    package init

---
+++ src/main/java/rs/crapp/dialgreen/domain/UserEntity.java
@@ -0,0 +1,42 @@
+package rs.crapp.dialgreen.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(table="user")
+public class UserEntity {
+
+    @Id
+    String id;
+
+    String name;
+
+    String points;
+
+    public UserEntity() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+}
