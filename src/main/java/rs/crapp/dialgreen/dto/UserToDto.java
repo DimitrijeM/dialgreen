8c7ea63feecb650c51681f19013da9a2d57af7db
dimitrije_dm <dimitrije.milenkovic@netcast.rs>
2018-01-27 12:01:33 +0100

    initial service method added

---
+++ src/main/java/rs/crapp/dialgreen/dto/UserToDto.java
@@ -0,0 +1,37 @@
+package rs.crapp.dialgreen.dto;
+
+public class UserToDto {
+
+    String id;
+
+    String name;
+
+    String point;
+
+    public UserToDto() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPoint() {
+        return point;
+    }
+
+    public void setPoint(String point) {
+        this.point = point;
+    }
+}
