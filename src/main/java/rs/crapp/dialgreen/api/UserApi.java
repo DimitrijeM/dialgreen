0f1e74d664b07ec372486f4344cb02f95d450e41
Milos Bijanic <bijanicmilos996@gmail.com>
2018-01-27 12:30:26 +0100

    finished api methods

--- src/main/java/rs/crapp/dialgreen/api/UserApi.java
+++ src/main/java/rs/crapp/dialgreen/api/UserApi.java
@@ -1,5 +1,6 @@
 package rs.crapp.dialgreen.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,20 +8,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import rs.crapp.dialgreen.dto.UserFromDto;
+import rs.crapp.dialgreen.dto.UserToDto;
+import rs.crapp.dialgreen.service.UserService;
+
 public class UserApi {
 	
-	//Create
-	@RequestMapping (value = "/file", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody UserToDto createNewFile(@RequestBody UserToDto user) {
-
+	@Autowired
+	UserService service;
+	
+	//User sing up
+	@RequestMapping (value = "/user/loging", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody UserToDto siginUser(@RequestBody UserFromDto user) {
+		return service.createUser(user);
 	}
 	
-	//READ - one
-	@RequestMapping UserToDto (value = "/file/{code}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody  returnFile(@PathVariable String code, HttpServletRequest request) {			
-	
+	//User login
+	@RequestMapping (value = "/user/singup", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody UserToDto logingUser(@RequestBody UserFromDto userName) {
+		return service.isUserExist(userName);
 	}
-
+	
+	//addPoints
+	@RequestMapping (value = "/user/addPoints", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody UserToDto addPoints(@RequestBody UserFromDto user, double weight, String materialType) {
+		return service.addPoints(user, weight, materialType);
+	}
 	
 	
 }
